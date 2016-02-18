a8480792763075fa98e1be7467d38e8c1db0723f
Sarah Reifeis <sreifeis@login2.bigred2.uits.iu.edu>
2016-02-18 15:32:04 -0500

    cleaned up factorization integrator

--- proto_md/integrators/factorization_integrator.py
+++ proto_md/integrators/factorization_integrator.py
@@ -26,20 +26,13 @@ class FactorizationIntegrator(integrator.Integrator):
         # next mean: average over steps - results in a n subsystem x n_cg array
         # flatten(): makes a 1D array 
         # [:,np.newaxis]: makes a 1xN column vector.
-
-	#avg_velocities = np.mean(np.mean(self.system.cg_velocities, axis = 0), axis = 1).flatten()[:,np.newaxis]
+	# this used to be: avg_velocities = np.mean(np.mean(self.system.cg_velocities, axis = 0), axis = 1).flatten()[:,np.newaxis]
 
 	# this is hackish ~ but more reliable than using the very chaotic velocities
-
 	avg_velocities = (self.system.cg_positions[0,:,-1,:] - self.system.cg_positions[0,:,0,:]) / \
 			(self.system.config["md_steps"]	- self.system.config["nstxout"])
 
 	avg_velocities = avg_velocities.flatten()[:,np.newaxis]
-
-	#avg_forces = (self.system.cg_velocities[0,:,-1,:] - self.system.cg_velocities[0,:,0,:]) / \
-        #                (self.system.config["md_steps"] - self.system.config["nstxout"])
-
-	#avg_forces = avg_forces.flatten()[:,np.newaxis]
 
 	# this is hackish. The 1000 factor should be replaced with 'dt' from the mdp file in case the user
 	# takes more than 1fs timestep
