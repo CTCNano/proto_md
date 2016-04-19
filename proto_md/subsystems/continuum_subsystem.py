f39ce784cd0054cac2b153e68c1a2d4848014efb
Andrew-AbiMansour <anabiman@indiana.edu>
2016-04-19 15:06:27 -0400

    fixed typos with some function calls

--- proto_md/subsystems/continuum_subsystem.py
+++ proto_md/subsystems/continuum_subsystem.py
@@ -97,16 +97,16 @@ class ContinuumSubsystem(subsystems.SubSystem):
 		CGv = np.zeros(self.nNodes)
 		CGf = np.zeros(self.nNodes)
 
-		CG[:self.NumNodes()], CGv[:self.NumNodes()], CGf[:self.NumNodes()] = self.ComputeCG_Pos(pos), self.ComputeCG_Vel(pos, vel), self.ComputeCG_For(pos, vel, forces)		
+		CG[:self.NumNodes()], CGv[:self.NumNodes()], CGf[:self.NumNodes()] = self.ComputeCG_Pos(pos), self.ComputeCG_Mom(pos, vel), self.ComputeCG_For(pos, vel, forces)		
 	else:
-	        CG, CGv, CGf = self.ComputeCG_Pos(pos), self.ComputeCG_Vel(pos, vel), self.ComputeCG_For(pos, vel, forces)
+	        CG, CGv, CGf = self.ComputeCG_Pos(pos), self.ComputeCG_Mom(pos, vel), self.ComputeCG_For(pos, vel, forces)
 
 	return CG, CGv, CGf
                 
     def minimized(self):
         pass
     
-    def equilibriated(self):
+    def equilibrated(self):
         """
         this is called just after the structure is equilibriated, this is the starting struct
         for the MD runs, this is to calculate basis.
